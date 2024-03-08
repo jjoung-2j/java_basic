5afaf6f20c6320268bab9e797094b382f540901e
jjoung-2j <145014345+jjoung-2j@users.noreply.github.com>
2024-03-09 04:57:32 +0900

    [update] velog

--- velog/src/day12_capsulation/Gujikja.java
+++ velog/src/day12_capsulation/Gujikja.java
@@ -3,6 +3,7 @@ package day12_capsulation;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Gujikja {
--- velog/src/my/day13_14/a/inheritance/CommonMember.java
+++ velog/src/my/day13_14/a/inheritance/CommonMember.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import my.util.MyUtil;
+import Util.MyUtil;
 
 public class CommonMember {
 	
