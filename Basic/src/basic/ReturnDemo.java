d007192517acb2b3bb275fa32d5e81c469abdd43
jjoung-2j <145014345+jjoung-2j@users.noreply.github.com>
2024-03-10 22:45:22 +0900

    [update] basic

---
+++ Basic/src/basic/ReturnDemo.java
@@ -0,0 +1,30 @@
+package basic;
+
+public class ReturnDemo {
+	/*public static String num(int i) {
+		if(i == 0) {
+			return " zero";
+		} else if (i == 1) {
+			return "one";
+		} else if (i == 2) {
+			return "two";
+		}
+		return "none";
+	}
+	
+	public static void main (String[] args) {
+		System.out.println(num(1));
+	}
+}*/
+	public static String[] getMembers() {
+		String[] members = {"지은", "혜정", "??"};
+		return members;
+	}
+	
+	public static void main(String [] args) {
+		String[] members = getMembers();
+		for (String member : members) {
+			System.out.println(member);
+		}
+	}
+}
---
+++ Basic/src/basic/VariableDemo.java
@@ -0,0 +1,36 @@
+package basic;
+
+public class VariableDemo {
+	public static void main(String[] args) {
+		
+		int a;
+		a = 1;
+		System.out.println(a+1);
+		
+		a = 2;
+		System.out.println(a+1);
+		
+		double b;
+		b = 1.1;
+		System.out.println(b+1.1);
+		
+		String first = "java";
+		// String first;
+		// first = "java";
+		/*
+		 안녕
+		 
+		 여러줄
+		 */
+		
+		System.out.println(first);
+		
+		String c, d;
+		c = "coding";
+		d = "everybody";
+		System.out.println(first+c+d);
+		
+		double e = 100.0;
+		System.out.println(e);
+	}
+}
